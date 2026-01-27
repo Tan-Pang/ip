3f3d10a0484239667e5f420e85009715e0fb5461
Tan-Pang <tanpang@u.nus.edu>
2026-01-27 14:57:37 +0800

    Added Level-2: Add, List

--- src/main/java/Happy.java
+++ src/main/java/Happy.java
@@ -3,20 +3,39 @@ import java.util.Arrays;
 
 public class Happy {
     public static void main(String[] args) {
-        String logo = " ____________________________________________________________\n"
-                + " Hello! I'm Happy!\n"
-                + " What can I do for you?\n"
-                + " ____________________________________________________________\n";
-        String byeLogo = " ____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + " ____________________________________________________________\n";
+        String logo = """
+                 ____________________________________________________________
+                 Hello! I'm Happy!
+                 What can I do for you?
+                 ____________________________________________________________
+                """;
+        String byeLogo = """
+                 ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                 ____________________________________________________________
+                 """;
         System.out.print(logo);
         String line;
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        String[] currList = new String[100];
+        int currIndex = 0;
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.print(" ____________________________________________________________\n" + line
-                    + " \n ____________________________________________________________\n");
+            if (line.equals("list")) {
+                currList = Arrays.copyOf(list, currIndex);
+                System.out.print(" ____________________________________________________________\n");
+                for (int i = 0; i < currIndex; i++) {
+                    System.out.printf("%d. %s%n", i+1, currList[i]);
+                }
+                System.out.print(" ____________________________________________________________\n");
+            } else {
+                System.out.print(" ____________________________________________________________\n"
+                        + " added: " + line
+                        + " \n ____________________________________________________________\n");
+                list[currIndex] = line;
+                currIndex++;
+            }
             line = in.nextLine();
         }
         System.out.print(byeLogo);

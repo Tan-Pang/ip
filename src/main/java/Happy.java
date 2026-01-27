ce10b7680e4c389e3096ee089c87d1e9f7c148ac
Tan-Pang <tanpang@u.nus.edu>
2026-01-27 19:47:01 +0800

    Added Level-3: Mark as Done
    Added Task.java

--- src/main/java/Happy.java
+++ src/main/java/Happy.java
@@ -4,36 +4,62 @@ import java.util.Arrays;
 public class Happy {
     public static void main(String[] args) {
         String logo = """
-                 ____________________________________________________________
-                 Hello! I'm Happy!
-                 What can I do for you?
-                 ____________________________________________________________
+                    ____________________________________________________________
+                    Hello! I'm Happy!
+                    What can I do for you?
+                    ____________________________________________________________
                 """;
         String byeLogo = """
-                 ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                 ____________________________________________________________
+                     ____________________________________________________________
+                     Bye. Hope to see you again soon!
+                     ____________________________________________________________
                  """;
         System.out.print(logo);
         String line;
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
-        String[] currList = new String[100];
+        Task[] list = new Task[100];
+        Task[] currList;
         int currIndex = 0;
         line = in.nextLine();
         while (!line.equals("bye")) {
             if (line.equals("list")) {
                 currList = Arrays.copyOf(list, currIndex);
-                System.out.print(" ____________________________________________________________\n");
+                System.out.print("""
+                                     ____________________________________________________________
+                                     Here are the tasks in your list:
+                                 """);
                 for (int i = 0; i < currIndex; i++) {
-                    System.out.printf("%d. %s%n", i+1, currList[i]);
+                    System.out.printf("    %d.[%s] %s%n", i + 1, currList[i].getStatusIcon(), currList[i].description);
                 }
-                System.out.print(" ____________________________________________________________\n");
+                System.out.print("    ____________________________________________________________\n");
+            } else if (line.startsWith("mark")) {
+                String[] words = line.split(" ");
+                int markIndex = Integer.parseInt(words[1]) - 1;
+                Task markItem = list[markIndex];
+                markItem.markAsDone();
+                System.out.print("""
+                             ____________________________________________________________
+                             Nice! I've marked this task as done:
+                         """);
+                System.out.printf("    [%s] %s%n", markItem.getStatusIcon(), markItem.description);
+                System.out.print("    ____________________________________________________________\n");
+            } else if (line.startsWith("unmark")) {
+                String[] words = line.split(" ");
+                int markIndex = Integer.parseInt(words[1]) - 1;
+                Task markItem = list[markIndex];
+                markItem.unmarkAsDone();
+                System.out.print("""
+                             ____________________________________________________________
+                             OK, I've marked this task as not done yet:
+                         """);
+                System.out.printf("    [%s] %s%n", markItem.getStatusIcon(), markItem.description);
+                System.out.print("    ____________________________________________________________\n");
             } else {
-                System.out.print(" ____________________________________________________________\n"
-                        + " added: " + line
-                        + " \n ____________________________________________________________\n");
-                list[currIndex] = line;
+                System.out.print("    ____________________________________________________________\n"
+                        + "    added: " + line
+                        + "\n    ____________________________________________________________\n");
+                Task t = new Task(line);
+                list[currIndex] = t;
                 currIndex++;
             }
             line = in.nextLine();

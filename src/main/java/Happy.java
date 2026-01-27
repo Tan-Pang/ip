fac43c9b45699e06e8bc537b4dcd1ea27b81b237
Tan-Pang <tanpang@u.nus.edu>
2026-01-27 14:33:53 +0800

    Added Level-1: Echo

--- src/main/java/Happy.java
+++ src/main/java/Happy.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Happy {
     public static void main(String[] args) {
         String logo = " ____________________________________________________________\n"
                 + " Hello! I'm Happy!\n"
                 + " What can I do for you?\n"
-                + " ____________________________________________________________\n"
+                + " ____________________________________________________________\n";
+        String byeLogo = " ____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
-                + " ____________________________________________________________";
-        System.out.println(logo);
+                + " ____________________________________________________________\n";
+        System.out.print(logo);
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.print(" ____________________________________________________________\n" + line
+                    + " \n ____________________________________________________________\n");
+            line = in.nextLine();
+        }
+        System.out.print(byeLogo);
     }
 }
